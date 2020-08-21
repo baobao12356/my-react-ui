b740e0592eea96b949d0efea4edf452823ebecf2
baobao12356 <747726294@qq.com>
2020-08-21 15:53:00 +0000

    Deploying to gh-pages from  @ 561dfe7278b5c64eaffc4b173df110e024101aa8 🚀

---
+++ precache-manifest.940dd48aff3c074f669d985301f1e3c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be238fb1e6972b987761706f01dc9e0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.bae6ff535b1ccc549e68.bundle.js"
+  },
+  {
+    "url": "runtime~main.bae6ff535b1ccc549e68.bundle.js"
+  },
+  {
+    "url": "vendors~main.bae6ff535b1ccc549e68.bundle.js"
+  },
+  {
+    "url": "vendors~main.bae6ff535b1ccc549e68.bundle.js.LICENSE.txt"
+  }
+]);
