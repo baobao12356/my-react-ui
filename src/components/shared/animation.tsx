9104df2400f70abe428e8bec3eb51a558868ebbb
懂你悲欢 <747726294@qq.com>
2020-09-02 23:12:03 +0800

    datapicker

--- src/components/shared/animation.tsx
+++ src/components/shared/animation.tsx
@@ -1,5 +1,6 @@
 
 // 对于贝塞尔曲线之类的可以单独拎出来放到shared的animation.tsx里。
+  
 import { keyframes } from "styled-components";
 
 export const easing = {
@@ -11,12 +12,35 @@ export const glow = keyframes`
   50% { opacity: .4; }
 `;
 
-
 export const iconSpin = keyframes`
   0% {
      transform: rotate(0deg);
   }
   100% {
      transform: rotate(360deg);
+  }
+`;
+
+export const modalOpenAnimate = keyframes`
+  0% {
+    opacity: 0;
+    transform: scaleY(0,0);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(1,1);
+    transform-origin: center;
+  }
+`;
+
+export const modalCloseAnimate = keyframes`
+  0% {
+    opacity: 0;
+    transform: scale(1,1);
+    transform-origin: center;
+  }
+  100% {
+    opacity: 0;
+    transform: scaleY(0,0);
   }
 `;
