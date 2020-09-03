50a56ecd98da1b2491719c2ce356bc88e91f948b
懂你悲欢 <747726294@qq.com>
2020-09-03 20:48:18 +0800

    提交daiam

--- src/components/datepicker/__test__/datepicker.test.tsx
+++ src/components/datepicker/__test__/datepicker.test.tsx
@@ -3,6 +3,9 @@ import { render, fireEvent, cleanup } from "@testing-library/react";
 import { DatePicker } from "../index";
 import { color, typography } from "../../shared/styles";
 
+
+
+
 describe("test DatePicker component", () => {
   it('render correctly', () => {
     const wrapper = render(
